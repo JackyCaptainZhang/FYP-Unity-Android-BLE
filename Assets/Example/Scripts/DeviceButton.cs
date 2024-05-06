27cf7c0a168660ee4d0188dad7ac3f1734571d0a
Jacky Zhang <dyzhjch@gmail.com>
2024-05-06 22:45:53 +0100

    1. Initial the git 2. Test the Connect and suscribe functions

---
+++ Assets/Example/Scripts/DeviceButton.cs
@@ -0,0 +1,85 @@
+﻿using Android.BLE;
+using Android.BLE.Commands;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeviceButton : MonoBehaviour
+{
+    private string _deviceUuid = string.Empty;
+    private string _deviceName = string.Empty;
+
+    [SerializeField]
+    private Text _deviceUuidText;
+    [SerializeField]
+    private Text _deviceNameText;
+
+    [SerializeField]
+    private Image _deviceButtonImage;
+    [SerializeField]
+    private Text _deviceButtonText;
+
+    [SerializeField]
+    private Color _onConnectedColor;
+    private Color _previousColor;
+
+
+    public static bool _isConnected = false;
+
+    private ConnectToDevice _connectCommand;
+    //private WriteToCharacteristic _writeToCharacteristic;
+    private ReadFromCharacteristic _readFromCharacteristic;
+    private SubscribeToCharacteristic _subscribeToCharacteristic;
+
+    public void Show(string uuid, string name)
+    {
+        _deviceButtonText.text = "Connect";
+
+        _deviceUuid = uuid;
+        _deviceName = name;
+
+        _deviceUuidText.text = uuid;
+        _deviceNameText.text = name;
+    }
+
+    public void Connect()
+    {
+        if (!_isConnected)
+        {
+            _connectCommand = new ConnectToDevice(_deviceUuid, OnConnected, OnDisconnected);
+            BleManager.Instance.QueueCommand(_connectCommand);
+        }
+        else
+        {
+            _connectCommand.Disconnect();
+        }
+    }
+
+
+    public void SubscribeToExampleService()
+    {
+
+        //Replace these Characteristics with YOUR device's characteristics
+        _subscribeToCharacteristic = new SubscribeToCharacteristic(_deviceUuid, "0000", "0001");
+        BleManager.Instance.QueueCommand(_subscribeToCharacteristic);
+    }
+
+    private void OnConnected(string deviceUuid)
+    {
+        _previousColor = _deviceButtonImage.color;
+        _deviceButtonImage.color = _onConnectedColor;
+
+        _isConnected = true;
+        _deviceButtonText.text = "Disconnect";
+
+        SubscribeToExampleService();
+    }
+
+    private void OnDisconnected(string deviceUuid)
+    {
+        _deviceButtonImage.color = _previousColor;
+
+        _isConnected = false;
+        _deviceButtonText.text = "Connect";
+    }
+}
---
+++ Assets/Example/Scripts/ExampleBleInteractor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using Android.BLE;
+using Android.BLE.Commands;
+using UnityEngine.Android;
+
+public class ExampleBleInteractor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _deviceButton;
+    [SerializeField]
+    private Transform _deviceList_item;
+    [SerializeField]
+    private Transform _deviceList;
+    [SerializeField]
+    private Transform _scanbutton;
+
+    [SerializeField]
+    private int _scanTime = 10;
+
+    [SerializeField]
+    public Camera cameraA;
+    [SerializeField]
+    public Camera cameraB;
+
+    private float _scanTimer = 0f;
+
+    private bool _isScanning = false;
+
+
+    public void ScanForDevices()
+    {
+        if (!_isScanning)
+        {
+            _isScanning = true;
+            BleManager.Instance.QueueCommand(new DiscoverDevices(OnDeviceFound, _scanTime * 1000));
+        }
+    }
+
+
+    private void Start()
+    {
+        cameraA.enabled = false;
+        cameraB.enabled = true;
+    }
+
+
+    private void Update()
+    {
+        if(_isScanning)
+        {
+            _scanTimer += Time.deltaTime;
+            if(_scanTimer > _scanTime)
+            {
+                _scanTimer = 0f;
+                _isScanning = false;
+            }
+        }
+
+        if (DeviceButton._isConnected) {
+            //_deviceList.gameObject.SetActive(false);
+            //_scanbutton.gameObject.SetActive(false);
+            //cameraA.enabled = true;
+            //cameraB.enabled = false;
+        }
+        else
+        {
+            //_deviceList.gameObject.SetActive(true);
+            //_scanbutton.gameObject.SetActive(true);
+            //cameraA.enabled = false;
+            //cameraB.enabled = true;
+        }
+    }
+
+    private void OnDeviceFound(string name, string device)
+    {
+        DeviceButton button = Instantiate(_deviceButton, _deviceList_item).GetComponent<DeviceButton>();
+        button.Show(name, device);
+       
+    }
+}
